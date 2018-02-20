77923af6487b696955a560b537e0679accaf8a0b
Laki16 <lkanen16@gmail.com>
2018-02-20 15:18:51 +0900

    temp

---
+++ Weapon/ArrowNormalAttack.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace TVNT
+{
+    public class ArrowNormalAttack : WeaponController
+    {
+        void Start()
+        {
+            GetComponent<Rigidbody>().AddForce(transform.forward * speed);
+        }
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.tag == "Monster")
+            {
+                //attack
+                other.GetComponent<TVNTCharacterController>().lives -= damage;
+            }
+        }
+    }
+}
