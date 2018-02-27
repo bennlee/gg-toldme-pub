8af3f4a44bf58e2e7e3bd1c6985aca95e3fd9eb7
Cradirow <hanorang2@naver.com>
2018-02-27 16:01:56 +0900

    create bugs

--- Weapon/ArrowNormalAttack.cs
+++ Weapon/ArrowNormalAttack.cs
@@ -1,54 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//namespace TVNT
-//{
-//    public class ArrowNormalAttack : WeaponController
-//    {
-//        float angle;
-//        Rigidbody rid;
-//        void Start()
-//        {
-//            //rid = GetComponent<Rigidbody>();
-//            //angle = Mathf.Atan2(rid.velocity.y, rid.velocity.x);
-//            //gameObject.transform.localEulerAngles = new Vector3(0, 0, (angle * 180) / Mathf.PI);
-//            //transform.LookAt(;
-//            GetComponent<Rigidbody>().AddForce(transform.forward * 150f);
 
-//        }
-//        private void OnTriggerEnter(Collider other)
-//        {
-//            if (other.tag == "Monster")
-//            {
-//                //attack
-//                other.GetComponent<TVNTCharacterController>().lives -= damage;
-//            }
-//        }
-//    }
-//}
 namespace TVNT
 {
-    public class ArrowNormalAttack : WeaponController
-    {
-        //        float angle;
-        //        Rigidbody rid;
-        //bool isPass = true;
-        void Start()
-        {
-            GetComponent<Rigidbody>().AddForce(transform.forward * 150f);
-        }
+	public class ArrowNormalAttack : WeaponController
+	{
+		void Start ()
+		{
+			gameObject.transform.Translate (Vector3.forward * 100);
+		}
 
-        private void OnTriggerEnter(Collider other)
-        {
-			if (other.GetComponent<TVNTCharacterController>().lives > 0)
-			{
-				other.GetComponent<TVNTCharacterController>().lives -= damage;
-				other.GetComponent<MonsterAIController>().threatenTime = 0;
+		private void OnTriggerEnter (Collider other)
+		{
+			if (other.tag == "Monster") {
+
+				if (other.GetComponent<TVNTCharacterController> ().lives > 0) {
+					Debug.Log ("bow attack!!!!");
+					//attack
+					other.GetComponent<TVNTCharacterController> ().lives -= damage;
+					other.GetComponent<MonsterAIController> ().threatenTime = 0;
+					GameObject.Destroy (gameObject);
+				} else {
+					other.GetComponent<MonsterAIController> ().CharacterDead ();
+				}
 			}
-			else
-			{
-				other.GetComponent<MonsterAIController>().CharacterDead();
-			}
-        }
-    }
+		}
+	}
 }
