03ad10a0e4999c96fac7d8e9c5214425a559a209
Cradirow <hanorang2@naver.com>
2018-03-09 17:47:49 +0900

    fix animation sync

--- AttackController.cs
+++ AttackController.cs
@@ -1,31 +1,31 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace TVNT{
 public class AttackController : MonoBehaviour {
 
-	GameObject hero;
-	// Use this for initialization
-	void Start () {
-hero = GameObject.Find("Knight");
-	}
+   GameObject hero;
+   // Use this for initialization
+   void Start () {
+     hero = GameObject.Find("Knight");
+   }
 
-	// Update is called once per frame
-	void Update () {
+   // Update is called once per frame
+   void Update () {
 
-	}
+   }
 
 
-	void Shoot(){
+   void Shoot(){
 
-		hero.transform.GetComponent<HeroController>().Shoot();
-		
-	}
+      hero.transform.GetComponent<HeroController>().Shoot();
 
-	void EndOfFight()
-	{
-		hero.transform.GetComponent<HeroController>().EndOfFight();
-	}
+   }
+
+   // void EndOfFight()
+   // {
+   //    hero.transform.GetComponent<HeroController>().EndOfFight();
+   // }
 }
 }
