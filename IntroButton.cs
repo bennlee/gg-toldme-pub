4153224b1cbd61480781491c4c61a2a878e7f377
Laki16 <lkanen16@gmail.com>
2018-03-10 16:11:09 +0900

    Update DoorScripts

---
+++ IntroButton.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TVNT
+{
+    public class IntroButton : MonoBehaviour
+    {
+        GameObject unit;
+
+
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(transform.GetChild(0).GetChild(0).childCount != 0)
+            {
+                unit = transform.GetChild(0).GetChild(0).GetChild(0).gameObject;
+
+                if(unit.tag == "Player")
+                {
+                    if(unit.GetComponent<HeroController>().isActivate == false)
+                    {
+                        unit.GetComponent<HeroController>().isActivate = true;
+                    }
+                    else
+                    {
+                        unit.GetComponent<HeroController>().HeroCloneDead();
+                    }
+                }
+
+            }
+        }
+    }
+}
