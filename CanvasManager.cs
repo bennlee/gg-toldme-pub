229e4224453e488f944949b2b2ecf4ccbe5716a5
Cradirow <hanorang2@naver.com>
2018-03-10 13:05:03 +0900

    add start scene

---
+++ CanvasManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasManager : MonoBehaviour {
+
+
+    public static CanvasManager instance = null;
+
+    public Canvas screen1_canvas;
+    public Canvas screen2_canvas;
+    public CanvasGroup screen1;
+    public CanvasGroup screen2;
+
+    void Awake(){
+        if(instance == null){
+            instance = this;
+        }else if(instance !=this){
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        screen1.alpha = 1;
+        screen1_canvas.enabled = true;
+
+        screen2.alpha = 0;
+        screen2_canvas.enabled = false;
+    }
+
+    public void screen1Active(){
+        screen1.alpha = 1;
+        screen1.interactable = true;
+        screen1_canvas.enabled = true;
+        screen2_canvas.enabled = false;
+    }
+
+    public void screen2Active(){
+        screen2.alpha = 1;
+        screen2.interactable = true;
+        screen1_canvas.enabled = false;
+        screen2_canvas.enabled = true;
+    }
+}
---
+++ Screen2Fade.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Screen2Fade : MonoBehaviour {
+
+	public void Fade()
+	{
+		StartCoroutine(DoFade());
+	}
+
+	IEnumerator DoFade()
+	{
+		CanvasGroup canvasGroup = GetComponent<CanvasGroup>();
+		while (canvasGroup.alpha > 0)
+		{
+			canvasGroup.alpha -= Time.deltaTime * 2;
+			yield return null;
+		}
+		canvasGroup.interactable = false;
+		yield return null;
+
+		CanvasManager.instance.screen1Active();
+	}
+}
