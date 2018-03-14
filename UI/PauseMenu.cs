76562689a501c49e5aecc56ecbdfddf9e0dddaa5
Cradirow <hanorange2naver.com>
2018-03-15 01:07:01 +0900

    Add pause funtions, light settings.
    Add option buttons and day, night image. Also entire map color is effected by day and night.

---
+++ UI/PauseMenu.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour {
+
+    public bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+    public GameObject topPanel;
+    public GameObject downPanel;
+
+    private void Awake()
+    {
+        topPanel.GetComponent<Image>().color = new Color(255, 255, 255, 255);
+        downPanel.GetComponent<Image>().color = new Color(255, 255, 255, 255);
+    }
+
+    public void Pause()
+    {
+        GameIsPaused = !GameIsPaused;
+
+        if (GameIsPaused)
+        {
+            pauseMenuUI.SetActive(true);
+            Time.timeScale = 0f;
+            topPanel.GetComponent<Image>().color = new Color(255, 255, 255, 0);
+            downPanel.GetComponent<Image>().color = new Color(255, 255, 255, 0);
+        }
+        else
+        {
+            pauseMenuUI.SetActive(false);
+            Time.timeScale = 1f;
+            topPanel.GetComponent<Image>().color = new Color(255, 255, 255, 255);
+            downPanel.GetComponent<Image>().color = new Color(255, 255, 255, 255);
+        }
+        
+    }
+    
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void QuitGame()
+    {
+        SceneManager.LoadScene("World");
+    }
+}
---
+++ Weapon/MagicSkillAttack.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TVNT
+{
+    public class MagicSkillAttack : WeaponController
+    {
+        void OnTriggerEnter(Collider other)
+        {
+            if (other.tag == "Monster")
+            {
+                if (other.GetComponent<TVNTCharacterController>().lives > 0)
+                {
+                    other.GetComponent<TVNTCharacterController>().lives -= damage;
+                    other.GetComponent<MonsterAIController>().threatenTime = 0;
+                    GameObject.Destroy(gameObject);
+                }
+                else
+                {
+                    //other.GetComponent<MonsterAIController>().CharacterDead();
+                    other.gameObject.SetActive(false);
+                }
+                GameObject.Destroy(gameObject);
+            }
+        }
+
+    }
+}
