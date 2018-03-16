a1fefa174f810c12a37569c4c0f6902511c0a3f6
Cradirow <hanorang2@naver.com>
2018-03-17 07:41:54 +0900

    Add camera movement.
    wasd : move
    mouse wheel : zoom in, out

--- DragMovement.cs
+++ DragMovement.cs
@@ -2,19 +2,20 @@
 using System.Collections;
 
 
-
 public class DragMovement : MonoBehaviour
 {
     int speed = 30;
 
-    // Use this for initialization
-    void Start()
+    private void Awake()
     {
-
+        //transform.position = new Vector3(Mathf.Clamp(transform.position.x, -70, 60),
+        //    Mathf.Clamp(transform.position.y, 30, 145), Mathf.Clamp(transform.position.z, -80, 50));
     }
+
     // Update is called once per frame
     void Update()
     {
+        
         if (Input.GetAxis("Mouse ScrollWheel") > 0)
         {
             GetComponent<Transform>().position =
@@ -26,11 +27,14 @@ public class DragMovement : MonoBehaviour
                 new Vector3(transform.position.x, transform.position.y + 1.0f, transform.position.z - .4f);
         }
         MoveObject();
+
     }
     void MoveObject()
     {
+
         float keyHorizontal = Input.GetAxis("Horizontal");
         float keyVertical = Input.GetAxis("Vertical");
+        
         transform.Translate(Vector3.right * speed * Time.smoothDeltaTime * keyHorizontal, Space.World);
         transform.Translate(Vector3.forward * speed * Time.smoothDeltaTime * keyVertical, Space.World);
     }
