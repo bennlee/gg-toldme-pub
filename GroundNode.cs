b540406f547346158a7fad7e05b2f52be90e5aab
Laki16 <lkanen16@gmail.com>
2018-03-17 21:14:44 +0900

    Monster Deploy fix

--- GroundNode.cs
+++ GroundNode.cs
@@ -10,40 +10,13 @@ namespace TVNT
 
         public GameObject prefab;
         GameObject hoverPrefab;
-
         GameObject MonsterIcon;
-        //GameObject MonsterIcon_2;
-        //GameObject MonsterIcon_3;
-        //GameObject MonsterIcon_4;
         bool isMouseOver = false;
 
-        // Use this for initialization
         void Start()
         {
             MonsterIcon = GameObject.FindGameObjectWithTag("MonsterIcon");
-            //MonsterIcon_2 = GameObject.FindGameObjectWithTag("MonsterIcon_2");
-            //MonsterIcon_3 = GameObject.FindGameObjectWithTag("MonsterIcon_3");
-            //MonsterIcon_4 = GameObject.FindGameObjectWithTag("MonsterIcon_4");
-
-            //prefab = GameObject.FindGameObjectWithTag("Slime");
-            //prefab.SetActive(false);
         }
-
-        // Update is called once per frame
-        void Update()
-        {
-            //if (!isMouseOver)
-            //{
-            //    OnMouseNotOver();
-            //}
-            //isMouseOver = false;
-        }
-
-        //public Vector3 GetDeployPosition()
-        //{
-        //    return;
-        //}
-
         void SelectMonster(GameObject selectedMonster)
         {
             MonsterIcon = selectedMonster;
@@ -52,37 +25,13 @@ namespace TVNT
         {
             if (MonsterIcon.GetComponent<MonsterSelect>().isMonsterSelected)
             {
-                Debug.Log(gameObject.transform.parent.parent.name + "MouseGO");
-                hoverPrefab = MonsterIcon.GetComponent<MonsterSelect>().hoverPrefab;
-                hoverPrefab.transform.position = gameObject.transform.position + new Vector3(0, 2, 0);
-                hoverPrefab.SetActive(true);
+                if (gameObject.transform.childCount == 0)
+                {
+                    hoverPrefab = MonsterIcon.GetComponent<MonsterSelect>().hoverPrefab;
+                    hoverPrefab.transform.position = gameObject.transform.position + new Vector3(0, 2, 0);
+                    hoverPrefab.SetActive(true);
+                }
             }
         }
-
-        //void OnMouseOver()
-        //{
-        //    isMouseOver = true;
-        //}
-        ////void OnMouseExit()
-        ////{
-        ////    if (MonsterIcon.GetComponent<MonsterSelect>().isMonsterSelected)
-        ////    {
-        ////        Debug.Log(gameObject.transform.parent.parent.name + "MouseExit");
-        ////        if (hoverPrefab.activeSelf)
-        ////        {
-        ////            hoverPrefab.SetActive(false);
-        ////        }
-        ////    }
-        ////}
-        //void OnMouseNotOver()
-        //{
-        //    if (MonsterIcon.GetComponent<MonsterSelect>().isMonsterSelected)
-        //    {
-        //        if (hoverPrefab.activeSelf)
-        //        {
-        //            hoverPrefab.SetActive(false);
-        //        }
-        //    }
-        //}
     }
 }
--- MonsterSelect.cs
+++ MonsterSelect.cs
@@ -8,30 +8,18 @@ namespace TVNT
     {
         public GameObject monsterController;
         int mapLayer;
-
         public GameObject prefab;
         public GameObject hoverPrefab;
-
-
         public bool isMonsterSelected;
 
-        // Use this for initialization
         void Start()
         {
             monsterController = GameObject.FindGameObjectWithTag("MonsterController");
-            //hoverPrefab = Instantiate(prefab);
-            //RemoveScriptsFromPrefab ();
-            //AdjustPrefabAlpha();
-            //hoverPrefab.SetActive(false);
+            hoverPrefab = Instantiate(prefab);
+            hoverPrefab.tag = "MonsterDeactivate";
+            hoverPrefab.SetActive(false);
             mapLayer = LayerMask.GetMask("Map");
         }
-
-        //void RemoveScriptsFromPrefab() {
-        //	Component[] components = hoverPrefab.GetComponentsInChildren<TurretTargettingSystem>();
-        //	foreach (Component component in components) {
-        //		Destroy (component);
-        //	}
-        //}
 
         void AdjustPrefabAlpha()
         {
@@ -43,86 +31,37 @@ namespace TVNT
             }
         }
 
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
+        //public static void ChangeLayersRecursively(this Transform trans, string name)
+        //{
+        //    trans.gameObject.layer = LayerMask.NameToLayer(name);
+        //    foreach(Transform child in trans)
+        //    {
+        //        ChangeLayersRecursively(child, name);
+        //    }
+        //}
 
         void IBeginDragHandler.OnBeginDrag(PointerEventData eventData)
         {
             // Debug.Log("Beginning drag");
             isMonsterSelected = true;
-            hoverPrefab = Instantiate(prefab);
-            hoverPrefab.SetActive(false);
-
+            //hoverPrefab = Instantiate(prefab);
+            //hoverPrefab.tag = "MonsterDeactivate";
+            //hoverPrefab.SetActive(false);
         }
 
         public void OnDrag(PointerEventData eventData)
         {
-            //// Debug.Log(eventData);
-            //RaycastHit[] hits;
-            //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            //hits = Physics.RaycastAll(ray, 150f, 1 << 8);
-            ////Debug.DrawLine(ray.origin, ray.direction, Color.blue, 150f);
 
-            ////Debug.DrawLine(Input.mousePosition + new Vector3(0, 30, 0), Input.mousePosition + new Vector3(0, -20, 0));
-            //if (hits != null && hits.Length > 0)
-            //{
-            //    int terrainCollderQuadIndex = GetTerrainColliderQuadIndex(hits);
-            //    if (terrainCollderQuadIndex != -1)
-            //    {
-            //        hoverPrefab.transform.position = hits[terrainCollderQuadIndex].transform.position + new Vector3(0, 2, 0);
-            //        hoverPrefab.SetActive(true);
-            //        // Debug.Log (hits [terrainCollderQuadIndex].point);
-            //    }
-            //    else
-            //    {
-            //        hoverPrefab.SetActive(false);
-            //    }
-            //}
-        }
-
-        int GetTerrainColliderQuadIndex(RaycastHit[] hits)
-        {
-            for (int i = 0; i < hits.Length; i++)
-            {
-                if (hits[i].transform.name.Equals("Base"))
-                {
-                    RaycastHit minimapRay;
-                    if (Physics.Raycast(hits[i].transform.position + new Vector3(0, -20, 0), hits[i].transform.position + new Vector3(0, -100, 0), out minimapRay, mapLayer))
-                    {
-                        if (minimapRay.transform.tag == "Map")
-                        {
-                            if (!minimapRay.transform.gameObject.GetComponent<MinimapCheck>().isHero)
-                            {
-                                return i;
-                            }
-                        }
-                    }
-                    //return i;
-                }
-            }
-            return -1;
         }
 
         public void OnEndDrag(PointerEventData eventData)
         {
-            // If the prefab instance is active after dragging stopped, it means
-            // it's in the arena so (for now), just drop it in.
-            //GameObject clone;
             if (hoverPrefab.activeSelf)
             {
-                //clone = Instantiate(prefab, hoverPrefab.transform.position, Quaternion.identity);
                 monsterController.GetComponent<MonsterController>().DeployMonster(prefab, hoverPrefab.transform.position, Quaternion.identity);
-                //clone.SetActive(true);
             }
-
-            // Then set it to inactive ready for the next drag!
-            // hoverPrefab.SetActive(false);
             Destroy(hoverPrefab);
             isMonsterSelected = false;
-            
         }
     }
 }
