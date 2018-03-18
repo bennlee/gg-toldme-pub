341481348dc31db47486d54bec29d206f6a1d291
Cradirow <hanorang2@naver.com>
2018-03-18 11:54:53 +0900

    Add speech controller fuctions.
    1. visible only when mainCamera y position is under 55.
    2. time between speak and non-speak.

---
+++ FamilyMoving.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FamilyMoving : MonoBehaviour {
+
+    GameObject hero = null;
+    float y_rotation;
+    
+	public void Look(GameObject c_hero)
+    {
+        hero = c_hero;
+    }
+
+    private void Update()
+    {
+        if(hero != null)
+        {
+            transform.LookAt(hero.transform);
+        }
+    }
+}
